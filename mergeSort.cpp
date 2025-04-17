7605c44369c4203c5d75bbe3f0f47c8b03fb91e3
KhoirulArifPratama <khoirularif932@gmail.com>
2025-04-17 10:37:26 +0700

    commit 2

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -22,5 +22,21 @@ void input()
     for (int i = 0; i < n; i++)
     {
         cout << "Array index ke - " << i << ": ";
+        cin >> arr[i];
     }
 }
+
+void mergeSort(int low, int high)
+{
+
+    if (low >= high)
+    {           // step1
+        return; // step1.a
+    }
+}
+
+int mid = (low + high) / 2; // step2
+
+// step3
+// fungsi rekursi - memanggil diri sendiri
+mergesort
