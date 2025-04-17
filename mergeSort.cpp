135a686a3cb24405e186047297523ee7c9e9b66e
KhoirulArifPratama <khoirularif932@gmail.com>
2025-04-17 11:17:09 +0700

    commit 8

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -74,4 +74,17 @@ void mergeSort(int low, int high)
         i++;
         k++;
     }
+    for (int x = low; x <= high; x++)
+    {
+        arr[x] = B[x];
+    }
+}
+void output()
+{
+    cout << "\ndata setelah diurutkan (MergeSort): ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 }
